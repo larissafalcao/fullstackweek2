2f89001fcc26c695de71beacbb0672968fa57019
Larissa Falcão <larissafalcao@outlook.com>
2021-06-01 21:37:45 -0300

    API update

--- api/my-invest/src/main/java/br/com/larissafalcao/myinvest/resources/CategoriaResource.java
+++ api/my-invest/src/main/java/br/com/larissafalcao/myinvest/resources/CategoriaResource.java
@@ -1,8 +1,48 @@
 package br.com.larissafalcao.myinvest.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.larissafalcao.myinvest.domain.Categoria;
+import br.com.larissafalcao.myinvest.repository.CategoriaRepository;
+
 @RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listartodas(){
+		return categoriaRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+		return categoriaRepository.findById(codigo).orElse(null);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	public void remover(@PathVariable Long codigo){
+		categoriaRepository.deleteById(codigo);
+	}
+	
+	@PostMapping
+	public Categoria criar(@RequestBody Categoria categoria){
+		return categoriaRepository.save(categoria);
+	}
+	
+	
 
 }
--- api/my-invest/src/main/java/br/com/larissafalcao/myinvest/resources/InvestimentoResource.java
+++ api/my-invest/src/main/java/br/com/larissafalcao/myinvest/resources/InvestimentoResource.java
@@ -3,6 +3,7 @@ package br.com.larissafalcao.myinvest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import br.com.larissafalcao.myinvest.repository.InvestimentoRepository;
 
 @RestController
 @RequestMapping("/investimentos")
+@CrossOrigin(origins = "http://localhost:3000")
 public class InvestimentoResource {
 	
 	@Autowired
@@ -31,7 +33,7 @@ public class InvestimentoResource {
 		return investimentoRepository.findById(codigo).orElse(null);
 	}
 	
-	@DeleteMapping
+	@DeleteMapping("/{codigo}")
 	public void remover(@PathVariable Long codigo) {
 		investimentoRepository.deleteById(codigo);
 		
